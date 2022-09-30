bd28574922b44bae6dca40c41d097f4179553235
vln4057a <vln4057a@u3-216-16.ens.fsi>
2022-09-30 09:46:43 +0200

    sauvegarde premier tp java , les classes

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,5 +1,50 @@
 package personnages;
 
-public class Gaulois {
+public class Gaulois 
+{
+	//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
+	
+	private String nom ;
+	private int force ;
+	private int effetpotion=1 ;
+	
 
+	public Gaulois(String nom, int force) 
+	{
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public String getNom () 
+	{
+		return nom ;
+	}
+	public void parler (String texte) 
+	{
+		System.out.println(prendreParole () + "<<" + texte + ">>");
+	}
+
+	private String prendreParole()
+	{
+		return "Le gaulois " + nom + ": ";
+		
+	}
+	
+	public void frapper (Romain romain )
+	{
+		System.out.println(nom + "envoie un grand coup dans la machoire de " + romain.getNom());
+		romain.recevoirCoup(force / 3) ;
+	}
+	
+	/*public String toString() 
+	{
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetpotion + "]";
+	}
+		
+	*/
+	public static void main(String[] args)
+	{
+
+	}
+		
 }
